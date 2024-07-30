e659219f226fde252072a80abac4c2558261b0d5
Rafael Guilherme <rafael_guilherme@hotmail.com>
2024-07-30 15:50:58 -0300

    Feat: Initial commit

---
+++ mongodb-spring-boot/src/main/java/com/rafaelguilherme/mongodb_spring_boot/MongodbSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.rafaelguilherme.mongodb_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongodbSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongodbSpringBootApplication.class, args);
+	}
+
+}
