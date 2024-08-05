0c5699b449c155f2fa8001255db320e52c4ff223
Rafael Guilherme <rafael_guilherme@hotmail.com>
2024-08-05 18:24:38 -0300

    Feat: delete user

--- mongodb-spring-boot/src/main/java/com/rafaelguilherme/mongodb_spring_boot/service/UserService.java
+++ mongodb-spring-boot/src/main/java/com/rafaelguilherme/mongodb_spring_boot/service/UserService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.rafaelguilherme.mongodb_spring_boot.domain.User;
+import com.rafaelguilherme.mongodb_spring_boot.dto.UserDTO;
 import com.rafaelguilherme.mongodb_spring_boot.repository.UserRepository;
 import com.rafaelguilherme.mongodb_spring_boot.service.exception.ObjectNotFoundException;
 
@@ -21,5 +22,18 @@ public class UserService {
 	
 	public User findById(String id) {
 		return userRepo.findById(id).orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return userRepo.insert(obj);
+	}
+	
+	public void delete(String id) {
+		findById(id);
+		userRepo.deleteById(id);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
