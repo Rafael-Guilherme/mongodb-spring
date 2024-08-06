12622b0579a126bc06cc5c1c5807b8228002e418
Rafael Guilherme <rafael_guilherme@hotmail.com>
2024-08-06 17:56:05 -0300

    Feat: Consulta simples com query methods

--- mongodb-spring-boot/src/main/java/com/rafaelguilherme/mongodb_spring_boot/service/PostService.java
+++ mongodb-spring-boot/src/main/java/com/rafaelguilherme/mongodb_spring_boot/service/PostService.java
@@ -1,5 +1,7 @@
 package com.rafaelguilherme.mongodb_spring_boot.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,5 +17,9 @@ public class PostService {
 	
 	public Post findById(String id) {
 		return postRepo.findById(id).orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public List<Post> findByTitle(String text) {
+		return postRepo.findByTitleContainingIgnoreCase(text);
 	}
 }
